091e5213969d726ad6122683edbec681b02269cf
LevonNahapetyan <levonnahapetyan@gmail.com>
2024-12-27 05:31:54 +0400

    second commit

--- StudentProject/src/Main.java
+++ StudentProject/src/Main.java
@@ -1,15 +1,129 @@
 import Model.Student;
 import Service.StudentService;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         StudentService service = new StudentService();
-        Student s1 = service.createStudent();
-        Student s2 = service.createStudent();
-        Student s3 = service.createStudent();
-        Student[] students = {s1, s2, s3};
-        service.printArrayInfo(students);
-//        s1.printInfo();
+
+//---------------------Adding students-------------------------
+//        Student s1 = service.createStudent();
+//        Student s2 = service.createStudent();
+//        Student s3 = service.createStudent();
+//        Student s4 = service.createStudent();
+//        Student s5 = service.createStudent();
+//        Student[] students = {s1, s2, s3};
+
+
+
+//---------------------Method Implementations-------------------
+
+//        service.printArrayInfo(students);
+//        service.printOldestStudent(students);
+//        service.printOldestInGirls(students);
+//        service.sortStudentsByMark(students);
+//        for(Student student : students) {
+//            student.printInfo();
+//        }
+
+
+
+//------------------------------------Menu---------------------------------------
+        boolean menu = true;
+        System.out.println("Tell us how many students are you going to create so we can create an array for you because for now our arrays are static");
+        int length = scanner.nextInt();
+        Student[] studentsForMenu = new Student[length];
+        int count = 0;
+        int i = 0;
+        while(menu == true && count <= length) {
+            System.out.println();
+            System.out.println();
+            System.out.println("-------Menu-------");
+            System.out.println("1: Create a student");
+            System.out.println("2: Print an info for chosen student");
+            System.out.println("3: Print info for an array of students");
+            System.out.println("4: Find the oldest student");
+            System.out.println("5: Find the oldest female student");
+            System.out.println("6: Sort students by mark from high to low");
+            System.out.println("7: Exit menu");
+
+            System.out.print("Enter the number: ");
+            int n = scanner.nextInt();
+            switch (n) {
+                case 1:
+                    StudentService studentService = new StudentService();
+                    Student student = studentService.createStudent();
+                    studentsForMenu[i] = student;
+                    i++;
+                    break;
+
+                case 2:
+                    System.out.println("Choose a student by name to print");
+                    String printCurrent = scanner.next();
+                    for(Student st : studentsForMenu) {
+                        if(st.getName().equals(printCurrent)){
+                            st.printInfo();
+                            break;
+                        }
+                    }
+                    break;
+
+                case 3:
+                    for(Student st : studentsForMenu) {
+                        if(st == null) {
+                            System.out.println("The array isn't full yet");
+                            break;
+                        } else {
+                            StudentService studentService1 = new StudentService();
+                            studentService1.printArrayInfo(studentsForMenu);
+                        }
+                    }
+                    break;
+
+                case 4:
+                    for(Student student1 : studentsForMenu){
+                        if(student1 == null){
+                            System.out.println("The array isn't full yet");
+                            break;
+                        } else{
+                            StudentService studentService1 = new StudentService();
+                            studentService1.printOldestStudent(studentsForMenu);
+                        }
+                    }
+                    break;
+
+                case 5:
+                    for(Student student1 : studentsForMenu){
+                        if(student1 == null){
+                        System.out.println("The array isn't full yet");
+                        break;
+                        } else {
+                            StudentService studentService1 = new StudentService();
+                            studentService1.printOldestInGirls(studentsForMenu);
+                        }
+                    }
+                    break;
+
+                case 6:
+                    for(Student student1 : studentsForMenu){
+                        if(student1 == null){
+                            System.out.println("The array isn't full yet");
+                            break;
+                        } else{
+                            StudentService studentService1 = new StudentService();
+                            studentService1.sortStudentsByMark(studentsForMenu);
+                        }
+                    }
+                    break;
+                case 7:
+                    menu = false;
+                    break;
+            }
+
+        }
+
 
 
     }
--- StudentProject/src/Service/StudentService.java
+++ StudentProject/src/Service/StudentService.java
@@ -31,15 +31,57 @@ public class StudentService {
 
         System.out.println("-------------");
         return student;
-
-
     }
 
 
     public void printArrayInfo(Student[] students) {
         for(Student student : students) {
             student.printInfo();
+        }
+    }
 
+
+    public void printOldestStudent(Student[] students) {
+        Student oldest = students[0];
+        for(int i = 1; i < students.length; i++) {
+            if(students[i].getYear() < oldest.getYear()) {
+                oldest = students[i];
+            }
+        }
+        System.out.println("The oldest student is " + oldest.getName() + ". He/she is " + (2024-oldest.getYear()) + " years old");
+    }
+
+
+    public void printOldestInGirls(Student[] students) {
+        Student oldest = null;
+        for(Student student : students) {
+            if(oldest == null) {
+                if(student.getGender() == 'F') {
+                    oldest = student;
+                }
+            } else if (student.getGender() == 'F' && student.getYear() < oldest.getYear()) {
+                oldest = student;
+            }
+        }
+        if(oldest != null) {
+            System.out.println("Oldest female student is " + oldest.getName());
+        } else {
+            System.out.println("No female students");
+        }
+    }
+
+
+
+    public void sortStudentsByMark(Student[] students){
+        Student temp = null;
+        for(int i = 0; i < students.length; i++) {
+            for(int j = 1; j < students.length; j++) {
+                if(students[j].getMark() > students[j-1].getMark()) {
+                    temp = students[j];
+                    students[j] = students[j-1];
+                    students[j-1] = temp;
+                }
+            }
         }
     }
 }
